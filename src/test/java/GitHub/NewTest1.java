a8efe28f5023ee41c62c6101ce5fc6dcb683785d
Harika-yenugu <harikanaidu624@gmail.com>
2024-07-15 16:06:15 +0530

    First Commit....

---
+++ src/test/java/GitHub/NewTest1.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("i want to became a software tester");
+	  
+  }
+}
